03be0b8b4e3455e2a2440e4f4c397b2ed53568c8
AyeMishDior <itsamish02@gmail.com>
2023-03-31 20:27:37 -0500

    final commit

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,9 +3,9 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 
-import { LOGIN_USER } from "../mutations";
+import { LOGIN_USER } from "../utils/mutations";
 import { useMutation } from "@apollo/client";
-import Auth from "../auth";
+import Auth from "../utils/auth";
 
 const MyLoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
